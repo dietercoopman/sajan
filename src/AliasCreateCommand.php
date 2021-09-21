affd4da6d4c8dfba8a9bbc019a3ae5fa726c046a
Dieter Coopman <dieter@deltasolutions.be>
2021-09-21 22:31:49 +0200

    alias commands

---
+++ src/AliasCreateCommand.php
@@ -0,0 +1,63 @@
+<?php namespace Dietercoopman\SajanPhp;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Process\Process;
+
+
+class AliasCreateCommand extends Command
+{
+
+
+    /**
+     * Configure the command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('alias:create')
+            ->setDescription('Create an alias for a program or command');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $question = new Question('<fg=green>Which program/command do you want to alias ?</> ', '');
+        $command  = $this->getHelper('question')->ask($input, $output, $question);
+
+        $question  = new Question('<fg=green>How do you want to name your alias ?</> ', '');
+        $aliasname = $this->getHelper('question')->ask($input, $output, $question);
+
+        $commandToRun = 'echo "alias ' . $aliasname . '=\"' . $command . '\"" >> ~/.bash_profile';
+        Process::fromShellCommandline($commandToRun)->mustRun();
+
+        $this->source();
+
+        return 0;
+    }
+
+    private function source()
+    {
+        $srccommand = 'if [ "$SHELL" == "/bin/zsh" ]; then
+    echo source ~/.bash_profile >~/.zshenv
+    source ~/.zshenv
+    exec zsh -l
+  else
+    source ~/.bash_profile
+    exec bash -l
+  fi';
+        Process::fromShellCommandline($srccommand)->mustRun();
+    }
+
+
+}
---
+++ src/AliasListCommand.php
@@ -0,0 +1,79 @@
+<?php namespace Dietercoopman\SajanPhp;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class AliasListCommand extends Command
+{
+
+
+    /**
+     * Configure the command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('alias:list')
+            ->setDescription('Get a table overview of all registered aliases on your computer');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $bashprofile = $this->getBashProfileFile();
+        $aliases     = $this->parseAliases($bashprofile);
+        $this->outputAsTable($output, $aliases);
+        return 0;
+    }
+
+    private function getBashProfileFile(): string
+    {
+        $disk = new Filesystem();
+        return $disk->get(getenv("HOME") . "/.bash_profile");
+    }
+
+    private function parseAliases(string $possibleAliasses): array
+    {
+        $possibleAliasses = collect(explode("\n", $possibleAliasses));
+        $aliases          = $possibleAliasses
+            ->filter(fn($line) => $this->isAnAlias($line))
+            ->transform(fn($alias) => $this->destructAlias($alias));
+
+        return $aliases->toArray();
+    }
+
+    private function destructAlias($aliasString): array
+    {
+        $aliasDestruct = explode('=', $aliasString);
+        $name          = Str::remove('alias ', $aliasDestruct[0]);
+        $command       = Str::remove('"', $aliasDestruct[1]);
+        return [$name, $command];
+    }
+
+    private function isAnAlias($line): bool
+    {
+        return Str::startsWith($line, 'alias');
+    }
+
+    private function outputAsTable(OutputInterface $output, array $aliases)
+    {
+        $table = new Table($output);
+
+        $table->setHeaders(['Alias', 'Command'])->setRows($aliases);
+        $table->render();
+    }
+
+}
