6110310a57efce30d947d9c0d65e2ba1b6012211
Dieter Coopman <dieter@deltasolutions.be>
2021-09-21 22:31:36 +0200

    secret command

---
+++ src/SecretCommand.php
@@ -0,0 +1,52 @@
+<?php namespace Dietercoopman\SajanPhp;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class SecretCommand extends Command
+{
+
+
+    /**
+     * Configure the command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('secret')
+            ->setDescription('Create a secure password');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $output->writeln('<fg=yellow>Let\'s tell you a little secret</>');
+        $output->writeln('<fg=green>' . $this->randomPassword() . '</>');
+        return 0;
+    }
+
+    public function randomPassword()
+    {
+        $alphabet    = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890=!?;';
+        $pass        = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 30; $i++) {
+            $n      = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
+    }
+
+
+}
