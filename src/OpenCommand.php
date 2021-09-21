b403d39730ac52d34e8c363c099c359b49dfac5b
Dieter Coopman <dieter@deltasolutions.be>
2021-09-21 22:31:57 +0200

    open dev tools command

---
+++ src/OpenCommand.php
@@ -0,0 +1,50 @@
+<?php namespace Dietercoopman\SajanPhp;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+
+class OpenCommand extends Command
+{
+
+
+    /**
+     * Configure the command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('open')
+            ->setDescription('Open phpstorm, finder and gittower for this folder');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $process = new Process(['pstorm', '.']);
+        $process->run();
+
+        $process = new Process(['gittower', '.']);
+        $process->run();
+
+        $process = new Process(['open', '.']);
+        $process->run();
+
+
+        $output->writeln('<fg=green>Happy coding</>');
+        return 0;
+    }
+
+
+}
